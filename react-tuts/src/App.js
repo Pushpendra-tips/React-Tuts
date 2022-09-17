af8ef4ebe7ea50eb1ad6e5f2314a5a3e42e1cb15
Pushpendra Tripathi <trippushpendra195@gmail.com>
2022-09-17 19:34:58 +0530

    CSS flex-direction

--- react-tuts/src/App.js
+++ react-tuts/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="wrap">
-      <h1>Center a Div Using Flexbox</h1>
-    </div>
+    <>
+      <div className="wrap">
+        <h1>Center a Div Using Flexbox</h1>
+      </div>
+
+      <div className="wrap">
+        <div className="box">Flexbox</div>
+        <div className="box">Flexbox</div>
+      </div>
+    </>
   );
 }
 
